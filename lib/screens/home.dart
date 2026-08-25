ac57e90d453c73707487e8f1c46d5926f3416ddc
Rhussu <wzaratesolar@gmail.com>
2026-08-25 00:17:15 -0400

    Primer Lobby

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:walatro/screens/setting.dart';
+import 'package:walatro/screens/lobby.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,8 +14,13 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
+            color: Colors.white,
             onPressed: () {
-              // Handle settings button press
+              // Navigate to the settings screen
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SettingScreen()),
+              );
             },
           ),
         ],
@@ -28,9 +35,32 @@ class HomeScreen extends StatelessWidget {
           // Contenido
           SafeArea(
             child: Center(
-              child: Text(
-                'Mi contenido',
-                style: TextStyle(color: Colors.white, fontSize: 30),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Navigate to the lobby screen
+                  Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          const LobbyScreen(),
+                      transitionDuration: Duration.zero,
+                      reverseTransitionDuration: Duration.zero,
+                    ),
+                  );
+                },
+                child: const Icon(
+                  Icons.play_arrow,
+                  size: 100,
+                  color: Colors.white,
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  padding: const EdgeInsets.all(50),
+                  backgroundColor:
+                      Colors.transparent, // Fondo azul con opacidad
+                ),
               ),
             ),
           ),
